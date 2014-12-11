31428900f3f6e5b66db32ad74ecda409abd2535b
Your Namels <edeleon4@mit.edu>
2014-12-11 17:01:56 -0500

    Initial Commit

---
+++ ScriptSpeak/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScriptSpeak
+//
+//  Created by Eduardo de Leon on 12/11/14.
+//  Copyright (c) 2014 Eduardo de Leon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
